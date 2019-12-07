13b2c8dd6f5dcffa558295005e1a3ce10e0cb64f
Gringo911 <irbisaltai@yandex.ua>
2019-12-07 22:51:33 +0300

    first commit

---
+++ src/Game.java
@@ -0,0 +1,13 @@
+/**
+ * Created by ВАСЯ on 24.11.2019.
+ */
+public class Game {
+
+    public static void main(String[] args) {
+        System.out.println("Hello User");
+        Krestik_Nolik start = new Krestik_Nolik();
+        start.game();
+
+
+    }
+}
---
+++ src/Krestik_Nolik.java
@@ -0,0 +1,22 @@
+/**
+ * Created by ВАСЯ on 24.11.2019.
+ */
+public class Krestik_Nolik {
+    //КОНСТАНТЫ ФИГУРЫ
+    public static final int DIMENSION = 3;
+    public static final int PUSTO = 0;
+    public static final int KHRESTYK = 1;
+    public static final int NOLYK = 2;
+    //КОНСТАНТЫ ТЕКУЩЕГО ПОЛОЖЕНИЯ
+    public static final int GAMA_PRODOLGENIE = 0;
+    public static final int GAMA_OVER_NICHIY = 1;
+    public static final int GAMA_OVER_POBEDA_KRESTIKA = 2;
+    public static final int GAMA_OVER_POBEDA_NOLIKA = 3;
+    //ПОЛЕ
+    public static final int RYD = 3, STOLB = 3;
+    public static String[][] field = new String[RYD][STOLB];
+
+
+    public void game() {
+    }
+}
